c035248571141f75af6cc87e772ddaea9dfdd65d
Munch42 <41879159+Munch42@users.noreply.github.com>
2025-03-10 15:33:15 -0600

    Finished ex3

--- ex3.py
+++ ex3.py
@@ -29,52 +29,88 @@ class Tree:
     def isEmpty (self):
         return self.root == None
     
-    def postorder (self, root):
-        if root != None:
+    def postorder(self, root):
+        if root:
             self.postorder(root.left)
             self.postorder(root.right)
-            print(root.data)
+            print(root.data, end=" ")
+
 
 def tokenize(expression):
-    tokens = argsInRaw.split()
+    tokens = expression.split()
     return tokens
 
+# extract_subexpression function suggested by ChatGPT after giving it my attempt at counting the 
+# parentheses
+def extract_subexpression(tokens, start_index):
+    """Extracts a subexpression enclosed in parentheses."""
+    open_parens = 0
+    for i in range(start_index, len(tokens)):
+        if tokens[i] == "(":
+            open_parens += 1
+        elif tokens[i] == ")":
+            open_parens -= 1
+            if open_parens == 0:
+                return tokens[start_index+1:i], i
+    return [], start_index  # If unmatched parentheses exist
+
 def parseTokensToTree(tokens):
-    if (len(tokens) == 1):
-        if (tokens[0].isdigit()):
-            # If the only token is a digit, return a tree with the topmost node being that number
-            return Tree(Node(tokens[0]))
-    
+    if len(tokens) == 1 and tokens[0].isdigit():
+        return Tree(Node(tokens[0]))
+
     index = 0
-    outputTree = None
-    leftTree = Tree()
-    rightTree = Tree()
-    for token in tokens:
+    leftTree = None  # Start with no tree
+
+    while index < len(tokens):
+        token = tokens[index]
         if token in "+-*/":
             tree = Tree(Node(token))
-            if tokens[index - 1].isdigit():
-                tree.root.setLeft(Node(tokens[index - 1]))
-            elif tokens[index - 1] == ")":
-                tree.root.setLeft(Node(leftTree.root))
-            
-            if tokens[index + 1].isdigit():
-                tree.root.setRight(Node(tokens[index + 1]))
-            """
-            IMPLEMENT THIS FOR WHEN THERE ARE EXPRESSIONS TO THE RIGHT OF AN OPERATOR
-            elif tokens[index + 1] == "(":
-                # This returns a tree
-                parseTokensToTree(tokens[index:])
-            """
 
-            leftTree = tree
+            # We handle the left operand
+            if index > 0: 
+                if tokens[index - 1].isdigit():
+                    tree.root.setLeft(Node(tokens[index - 1]))
+                elif tokens[index - 1] == ")" and leftTree:
+                    # Set the left side of our new tree to the tree we have been building up
+                    tree.root.setLeft(leftTree.root)
+
+            # We handle the right operand
+            if index + 1 < len(tokens):
+                if tokens[index + 1].isdigit():
+                    tree.root.setRight(Node(tokens[index + 1]))
+                elif tokens[index + 1] == "(":
+                    subexpr, new_index = extract_subexpression(tokens, index + 1)
+                    tree.root.setRight(parseTokensToTree(subexpr).root)
+                    index = new_index 
+
+            leftTree = tree  # Update leftTree for the next operator we find
         index += 1
-        
-    outputTree = leftTree
-    return outputTree
 
-    
+    return leftTree if leftTree else Tree(Node(tokens[0]))
+
+def evaluateTree(root):
+    if root:
+        evaluateTree(root.left)
+        evaluateTree(root.right)
+        if root.left != None and root.right != None:
+            if root.data == "+":
+                root.data = float(root.left.data) + float(root.right.data)
+            if root.data == "-":
+                root.data = float(root.left.data) - float(root.right.data)
+            if root.data == "*":
+                root.data = float(root.left.data) * float(root.right.data)
+            if root.data == "/":
+                root.data = float(root.left.data) / float(root.right.data)
+        
+        if root.parent == None:
+            if root.data == 0:
+                return 0
+            if type(root.data) == float:
+                if root.data.is_integer():
+                    return int(root.data)
+            return root.data
 
 argsInRaw = " ".join(sys.argv[1:])
 tokens = tokenize(argsInRaw)
 outputTree = parseTokensToTree(tokens)
-print(outputTree.postorder(outputTree.root))
+print(evaluateTree(outputTree.root))
